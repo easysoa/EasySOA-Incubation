a123f74fc7a1f580072f41957d1050cac43d792b
Marc Dutoo <marc.dutoo@openwide.fr>
2012-12-03 10:25:06 +0100

    axxx apv : improved comments

--- easysoa-samples/easysoa-samples-axxx/axxx-dps-apv/src/main/java/com/axxx/dps/apv/ws/ProjetWebService.java
+++ easysoa-samples/easysoa-samples-axxx/axxx-dps-apv/src/main/java/com/axxx/dps/apv/ws/ProjetWebService.java
@@ -5,10 +5,22 @@ import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
+/**
+ * Manages the lifecycle of APV Web "Projets" (requests for funded holidays)
+ * 
+ * @author mdutoo
+ *
+ */
 @WebService(name="ProjetService", // used for imported WSDL, else default to java itf
 	targetNamespace="http://www.axxx.com/dps/apv") // default to package ex. http://ws.apv.dps.axxx.com/
 public interface ProjetWebService {
-    
+   
+	/**
+	 * Publishes the given projet to business reporting and DCV Pivotal CRM
+	 * 
+	 * @param projetId the projet to publish
+	 * @return true if successful, false otherwise
+	 */
 	@WebMethod @WebResult(name="success", targetNamespace="http://www.axxx.com/dps/apv")
     public boolean publish(@WebParam(name="projetId") String projetId);
 
--- easysoa-samples/easysoa-samples-axxx/axxx-dps-apv/src/main/java/com/axxx/dps/apv/ws/ProjetWebServiceImpl.java
+++ easysoa-samples/easysoa-samples-axxx/axxx-dps-apv/src/main/java/com/axxx/dps/apv/ws/ProjetWebServiceImpl.java
@@ -7,6 +7,15 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 
+/**
+ * Implementation of the APV ProjetService on top of the APV Web
+ * Spring + Hibernate + CXF stack.
+ * 
+ * For now this is only a mock implementation.
+ * 
+ * @author mdutoo
+ *
+ */
 @WebService(name="ProjetService", // service interface name (portType), else defaults to java interface
 	targetNamespace="http://www.axxx.com/dps/apv", // else defaults to java package ex. http://ws.apv.dps.axxx.com/
 	serviceName="ProjetServiceImpl") // service implementation name (used for binding & port), else default to java impl
@@ -15,9 +24,15 @@ public class ProjetWebServiceImpl implements ProjetWebService {
 
    	private Log log = LogFactory.getLog(this.getClass());
    	
+   	/**
+   	 * Mock implementation, return true if id is more than 3
+   	 */
 	@Override
 	public boolean publish(String projetId) {
 		log.debug("publish start");
+		if (Integer.parseInt(projetId) > 3) {
+			return true;
+		}
 		return false;
 	}
 

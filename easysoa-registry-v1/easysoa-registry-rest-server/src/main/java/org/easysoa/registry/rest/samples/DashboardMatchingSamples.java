cfcd8eed855713fd432deaaae0a422fee52f1b7d
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-12-07 18:07:41 +0100

    Improve samples to make some suggestions emerge in dashboard

--- easysoa-registry-v1/easysoa-registry-rest-server/src/main/java/org/easysoa/registry/rest/samples/DashboardMatchingSamples.java
+++ easysoa-registry-v1/easysoa-registry-rest-server/src/main/java/org/easysoa/registry/rest/samples/DashboardMatchingSamples.java
@@ -61,32 +61,50 @@ public class DashboardMatchingSamples {
 
 		// Create information services
 		SoaNodeId serviceId = null;
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 4; i++) {
 			p.put(InformationService.XPATH_TITLE, "Java Service #" + i);
 			p.put(InformationService.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portType" + i);
 			serviceId = new SoaNodeId(InformationService.DOCTYPE, 
-					new InformationServiceName(ServiceNameType.WEB_SERVICE, "namespace", "itf" + i).toString());
+					new InformationServiceName(ServiceNameType.WEB_SERVICE, "namespace", "portType" + i).toString());
 			postSoaNode(serviceId, p);
 		}
-		
+
+        p.put(InformationService.XPATH_TITLE, "Java Service #3 Bis");
+        p.put(InformationService.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portType3");
+        postSoaNode(new SoaNodeId(InformationService.DOCTYPE, 
+                new InformationServiceName(ServiceNameType.WEB_SERVICE, "namespace", "portType3bis").toString()), p);
+        
 		// Components
 		p.put(Component.XPATH_COMP_LINKED_INFORMATION_SERVICE, (String) getSoaNode(serviceId).getProperty(SoaNode.XPATH_UUID));
 		postSoaNode(new SoaNodeId(Component.DOCTYPE, "Service 2 Component"), p, javaPlatformId);
 		
-		// Create service impl that matches an IS
+		// Create service impls that matches an IS
         p.put(ServiceImplementation.XPATH_LANGUAGE, "Java");
         p.put(ServiceImplementation.XPATH_BUILD, "Maven");
         p.put(ServiceImplementation.XPATH_SERVICE_LANGUAGE, "JAXRS");
         p.put(ServiceImplementation.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portType2");
         postSoaNode(new SoaNodeId(ServiceImplementation.DOCTYPE, "Java Service Impl 2"), p);
+        
+        p.put(ServiceImplementation.XPATH_LANGUAGE, "Java");
+        p.put(ServiceImplementation.XPATH_BUILD, "Maven");
+        p.put(ServiceImplementation.XPATH_SERVICE_LANGUAGE, "JAXRS");
+        p.put(ServiceImplementation.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portType2");
+        postSoaNode(new SoaNodeId(ServiceImplementation.DOCTYPE, "Java Service Impl 2 Bis"), p);
 
-        // Create service impls that doesn't match an IS
+        // Create impl that matches several IS (no direct link, but suggested through dashboard)
         p.put(ServiceImplementation.XPATH_LANGUAGE, "Java");
         p.put(ServiceImplementation.XPATH_BUILD, "Maven");
         p.put(ServiceImplementation.XPATH_SERVICE_LANGUAGE, "JAXRS");
         p.put(ServiceImplementation.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portType3");
         postSoaNode(new SoaNodeId(ServiceImplementation.DOCTYPE, "Java Service Impl 3"), p);
-
+        
+        // Create service impls that doesn't match an IS
+        p.put(ServiceImplementation.XPATH_LANGUAGE, "Java");
+        p.put(ServiceImplementation.XPATH_BUILD, "Maven");
+        p.put(ServiceImplementation.XPATH_SERVICE_LANGUAGE, "JAXRS");
+        p.put(ServiceImplementation.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portType4");
+        postSoaNode(new SoaNodeId(ServiceImplementation.DOCTYPE, "Java Service Impl 4"), p);
+        
         p.put(ServiceImplementation.XPATH_SERVICE_LANGUAGE, "JAXWS");
         p.put(ServiceImplementation.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portTypeJaxWs");
         p.put(ServiceImplementation.XPATH_WSDL_SERVICE_NAME, "{namespace}serviceJaxWs");
@@ -96,18 +114,16 @@ public class DashboardMatchingSamples {
         p.put(Endpoint.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portTypeJaxWs");
         p.put(Endpoint.XPATH_WSDL_SERVICE_NAME, "{namespace}serviceJaxWs");
         postSoaNode(new EndpointId("Production", "http://www.endpoint.com/serviceJaxWs"), p);
-
+        
         // Create endpoint that matches no impl but matches an IS (placeholder impl)
         p.put(Endpoint.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portType0");
         p.put(Endpoint.XPATH_WSDL_SERVICE_NAME, "{namespace}service0");
         postSoaNode(new EndpointId("Production", "http://www.endpoint.com/service0"), p);
 
-        // Create endpoint that matches nothing
-        p.put(Endpoint.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portType9");
-        p.put(Endpoint.XPATH_WSDL_SERVICE_NAME, "{namespace}service9");
-        postSoaNode(new EndpointId("Production", "http://www.endpoint.com/service9"), p);
-		
-        // TODO Better coverage
+        // Create endpoint that matches several impls
+        p.put(Endpoint.XPATH_WSDL_PORTTYPE_NAME, "{namespace}portType2");
+        p.put(Endpoint.XPATH_WSDL_SERVICE_NAME, "{namespace}service2");
+        postSoaNode(new EndpointId("Production", "http://www.endpoint.com/service2"), p);
 	}
 	
 	public Client createAuthenticatedHTTPClient() {

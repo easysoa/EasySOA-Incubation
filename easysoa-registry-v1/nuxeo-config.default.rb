f767be1968b661deb9653cfc1d479248c568496e
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2013-04-05 11:15:44 +0200

    New version of registry packaging/deployment script

---
+++ easysoa-registry-v1/nuxeo-config.default.rb
@@ -0,0 +1,7 @@
+# Target custom Nuxeo
+#NUXEO_PATH = '/home/username/nuxeo-cap-5.6.0-HF06-tomcat/'
+
+# Distrib config
+PACKAGE_NAME = 'easy-soa'
+PROJECT_NAME = 'easysoa-registry-marketplace'
+PROJECT_VERSION = '1.0-SNAPSHOT' # TODO Extract value from filename

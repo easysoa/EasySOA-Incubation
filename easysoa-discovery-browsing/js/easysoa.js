5284d1a5fc3ba720fb29f39af4a8f85fecc513cb
Jeremie Guillemotte <jeremie.guillemotte@openwide.fr>
2013-04-26 11:36:14 +0200

    Add interceptor function EasySOA.js file to set dynamically the host and port when the bookmarklet.js file is requested.

--- easysoa-discovery-browsing/js/easysoa.js
+++ easysoa-discovery-browsing/js/easysoa.js
@@ -8,6 +8,7 @@
 require('longjohn');
 var http = require('http');
 var express = require('express');
+var fs = require('fs');
 
 var settings = require('./settings');
 var authComponent = require('./auth');
@@ -48,6 +49,19 @@ app.configure(function(){
   app.use(express.favicon(settings.WWW_PATH + '/favicon.ico'));
   app.use(express.static(settings.WWW_PATH));
   app.use(express.directory(settings.WWW_PATH));
+
+  // To set dynamically the host and port in the bookmarklet template to generate the bookmarklet script  
+  app.get('/js/bookmarklet/bookmarklet-min.js', function(req, res) {
+    var jsFile = fs.readFileSync("../www/js/bookmarklet/bookmarklet.template","utf8");
+    //console.log("jsFile : " + jsFile);
+    //console.log("#{host} = "  +  webServer.address().address);
+    //console.log("#{port} = "  +  webServer.address().port);
+    jsFile = jsFile.replace("#{host}", webServer.address().address);
+    jsFile = jsFile.replace("#{port}", webServer.address().port);
+    //console.log("jsFile : " + jsFile);
+    res.writeHead(200);
+    res.end(jsFile);
+  });
     
 });
 webServer.listen(settings.WEB_PORT);
@@ -58,7 +72,7 @@ nuxeoComponent.startConnectionChecker();
  * Set up proxy server
  */
 var proxyServer = http.createServer(function(request, response) {
-  
+		
   // Service finder
   dbbComponent.handleProxyRequest(request, response);
   

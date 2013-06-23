4319cdaed2c10217970420c935a9c8d131fd0a16
Marc Dutoo <marc.dutoo@openwide.fr>
2013-06-23 23:16:17 +0200

    web disco : support by node of 64 as well as 32 bit archi

--- easysoa-discovery-browsing/start-web.sh
+++ easysoa-discovery-browsing/start-web.sh
@@ -8,7 +8,18 @@ echo " Default proxy configuration: Host=127.0.0.1, Port=8081)"
 echo "DEPENDENCIES: Service registry (to log in)"
 echo $LINE
 
+# Checking 32 or 64 bits
+# NB. not using uname which could be wrong in chrooted envs
+# see http://stackoverflow.com/questions/106387/is-it-possible-to-detect-32-bit-vs-64-bit-in-a-bash-script
+if [ `getconf LONG_BIT` = "64" ]
+then
+    echo "Using 64-bit node"
+    NODE=../node64
+else
+    echo "Using 32-bit node"
+    NODE=../node
+fi
 cd js
-../node -v
-../node --debug easysoa.js
+$NODE -v
+$NODE --debug easysoa.js
 

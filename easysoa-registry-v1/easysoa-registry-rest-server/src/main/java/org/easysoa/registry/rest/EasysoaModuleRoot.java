175a6c76e3669d1d24af5db58a2cd4a27944df0a
Jeremie Guillemotte <jeremie.guillemotte@openwide.fr>
2013-08-30 09:48:23 +0200

    New method getServerUrl() added

--- easysoa-registry-v1/easysoa-registry-rest-server/src/main/java/org/easysoa/registry/rest/EasysoaModuleRoot.java
+++ easysoa-registry-v1/easysoa-registry-rest-server/src/main/java/org/easysoa/registry/rest/EasysoaModuleRoot.java
@@ -1,20 +1,20 @@
 /**
  * EasySOA Registry
  * Copyright 2011-2013 Open Wide
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Contact : easysoa-dev@googlegroups.com
  */
 
@@ -39,22 +39,22 @@ import org.nuxeo.runtime.api.Framework;
 
 /**
  * Base class for EasySOA controllers.
- * 
+ *
  * Provides helper methods & props to all Freemarker templates & controllers.
- * 
+ *
  * @author jguillemotte, mdutoo
  */
 public class EasysoaModuleRoot extends ModuleRoot {
 
     /**
-     * 
+     *
      * @return The current user name
-     * @throws Exception 
+     * @throws Exception
      */
     public String getCurrentUser() throws Exception {
         String user = "";
         Principal currentUser = ctx.getPrincipal(); // SessionFactory.getSession(request).getPrincipal()
-        
+
         if(currentUser != null){
             user = currentUser.getName();
         }
@@ -64,15 +64,15 @@ public class EasysoaModuleRoot extends ModuleRoot {
     /**
      * Provides it to Freemarker templates.
      * TODO LATER do it in a nuxeo service's init
-     * @returns (LATER nuxeo.conf's nuxeo.url else) VirtualHostHelper's host
+     * @returns (LATER nuxeo.conf's nuxeo.url else) VirtualHostHelper's host if not localhost
      * else returns guessed server hostname
      * (as known locally) to provide to non-local clients
      */
     public String getServerHost() {
     	// NB. how to get nuxeo.conf's nuxeo.url ?? not from org.nuxeo.common.Environment.getDefault()...
-    	
+
     	// Trying nuxeo's VirtualHostHelper
-    	// NB. handles proxying but doesn't guess locally known hostname/IP 
+    	// NB. handles proxying but doesn't guess locally known hostname/IP
     	String serverUrlString = VirtualHostHelper.getServerURL(request);
     	try {
 			URL serverUrl = new URL(serverUrlString);
@@ -87,11 +87,40 @@ public class EasysoaModuleRoot extends ModuleRoot {
     	// else falling back to guessing host from network
     	return NetworkUtil.guessNetworkHost();
     }
-    
+
+    /**
+     * Provides it to Freemarker templates.
+     * TODO LATER do it in a nuxeo service's init
+     * @returns (LATER nuxeo.conf's nuxeo.url else) VirtualHostHelper's url if not localhost
+     * else returns it but with guessed server hostname
+     * (as known locally) to provide to non-local clients
+     */
+    public String getServerUrl() {
+    	// NB. how to get nuxeo.conf's nuxeo.url ?? not from org.nuxeo.common.Environment.getDefault()...
+
+    	// Trying nuxeo's VirtualHostHelper
+    	// NB. handles proxying but doesn't guess locally known hostname/IP
+    	String serverUrlString = VirtualHostHelper.getServerURL(request);
+    	try {
+			URL serverUrl = new URL(serverUrlString);
+			String serverHost = serverUrl.getHost();
+			if (!serverHost.equals("localhost") && !serverHost.startsWith("127.")) {
+				return serverUrlString;
+			}
+
+            // else falling back to guessing host from network
+            return serverUrl.getProtocol() + "://" + NetworkUtil.guessNetworkHost() + ':' + serverUrl.getPort() + '/';
+
+		} catch (MalformedURLException e) {
+			// silent failure
+		}
+        return "[failed to guess server url]";
+    }
+
     public boolean isDevModeSet() {
     	return Framework.isDevModeSet();
     }
-    
+
     /**
      * Provides EasySOA properties to Freemarker templates (though better use dedicated getters)
      * @return
@@ -104,32 +133,32 @@ public class EasysoaModuleRoot extends ModuleRoot {
 			throw new RuntimeException("Unable to get DocumentService", e);
 		}
     }
-    
+
     public String getWebDiscoveryUrl() {
     	return getProperties().getProperty("webDiscovery.url", "http://" + getServerHost() + ":8083/");
     }
-    
+
     public String getJasmineUrl() {
     	return getProperties().getProperty("jasmine.url", "http://" + getServerHost() + ":9100/jasmine/");
     }
-    
+
     //protected String subprojectCriteria = // TODO cache
-    
+
     public DocumentService getDocumentService() throws Exception {
     	return Framework.getService(DocumentService.class);
     }
     public UserManager getUserManager() throws Exception {
     	return Framework.getService(UserManager.class);
     }
-    
+
     public String buildSubprojectCriteria(DocumentModel soaNodeDocModel,
     		String subprojectId) throws PropertyException, ClientException {
     	return NXQLQueryHelper.buildSubprojectCriteria(
     			soaNodeDocModel.getCoreSession(), subprojectId, true);
     }
-    
+
     public SubprojectId parseSubprojectId(String subprojectId) throws Exception {
     	return SubprojectServiceImpl.parseSubprojectId(subprojectId);
     }
-    
+
 }
